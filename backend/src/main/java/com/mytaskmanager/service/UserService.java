652abe656970063ea9d143778149313a87250b07
Abdelabset <Abdelbaset.Abidi@studmail.w-hs.de>
2024-02-03 19:45:14 +0100

    connect with Frontend

--- backend/src/main/java/com/mytaskmanager/service/UserService.java
+++ backend/src/main/java/com/mytaskmanager/service/UserService.java
@@ -1,4 +1,13 @@
 package com.mytaskmanager.service;
 
-public class UserService {
+import org.springframework.stereotype.Service;
+
+public interface UserService {
+
+//    void saveUser(UserDto userDto);
+//
+//    User findUserByEmail(String email);
+//
+//    List<UserDto> findAllUsers();
+
 }
